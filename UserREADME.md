31b2db86b2fdc2141a0ac0a141b17fcbbc1fad3c
AshPap <asherpappas@gmail.com>
2021-02-23 12:12:33 -0700

    adding files to repo

---
+++ UserREADME.md
@@ -0,0 +1,28 @@
+
+  
+  
+  # README generator
+  ![badge](https://img.shields.io/badge/license-MIT-green)
+  ## Description
+  Using npm and node this prompt will generate a README for your project
+  # Table of Contents
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [License](#License)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [Questions](#Questions)
+  ## Installation
+  npm install in the terminal then input node index.js to begin the prompt
+  ## Usage
+  node inex.js in the terminal will begin the prompt, then fill out the information for each line
+  ## License
+  MIT License <br>Copyright (c) 2021, Asher Pappas <br>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: <br>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. <br>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ## Contributing
+  no contributing at this time
+  ## Tests
+  no tests
+  ## Questions
+  If you have any questions please reach out through email or Github. <br>
+  My email is asherpappas@gmail.com <br>
+  My Github can be accessed [here](https://github.com/asherpappas)
---
+++ index.js
@@ -0,0 +1,85 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+// array of questions for user
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    },
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    },
+    {
+    type: 'input',
+    name: 'github',
+    message: 'What is your Github username?',
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email?',
+    },
+    {
+    type: 'confirm',
+    name: 'askLogo',
+    message: 'Do you have a logo for the top of your README?',
+    default: true,
+    },
+    {
+    type: 'input',
+    name: 'logoURL',
+    message: 'What is the URL for the logo?',
+    when: function (answers) {
+        return answers.askLogo;
+    },
+    },
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Give a brief description about this project.'
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message: 'Give instructions on how to install about your project.'
+    },
+    {
+    type: 'input',
+    name: 'usage',
+    message: 'Give instructions on how to use your project.'
+    },
+    {
+    type: 'list',
+    name: 'license',
+    message: 'Which license do you want to use?',
+    choices: ['BSD', 'MIT', 'GPL'],
+    },
+    {
+    type: 'input',
+    name: 'contributing',
+    message: 'What are your guidelines for contributing?'
+    },
+    {
+    type: 'input',
+    name: 'testing',
+    message: 'What are your instructions for testing?'
+    },
+
+]
+
+// function to initialize program
+function init(questions) {
+    inquirer.prompt(questions).then(answers => {
+        fs.writeFile('UserREADME.md', generateMarkdown(answers), (err) => 
+            err ? console.error(err) : console.log('Your UserREADME has been created!'))
+        })
+}
+
+// function call to initialize program
+init(questions);
